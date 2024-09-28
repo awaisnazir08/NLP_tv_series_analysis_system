d143de86602ae86329d5ea59c6df8934402a90e4
awaisnazir08 <owaisnazir2228@gmail.com>
2024-09-28 15:56:40 +0500

    classify jutsu complete

--- text_classification/jutsu_classifier.py
+++ text_classification/jutsu_classifier.py
@@ -1,11 +1,14 @@
 import torch
 import huggingface_hub
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding, TrainingArguments, pipeline
 from datasets import Dataset
 import pandas as pd 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from .cleaner import Cleaner
+import gc
+from .training_utils import get_class_weights, compute_metrics
+from .custom_trainer import CustomTrainer
 
 class JutsuClassifier():
     def __init__(self, model_path, data_path=None, text_column_name='text', label_column_name='jutsu', model_name='distilbert/distilbert-base-uncased', test_size=0.2, num_labels=3,huggingface_token = None):
@@ -18,6 +21,7 @@ class JutsuClassifier():
         self.num_labels = num_labels
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.huggingface_token = huggingface_token
+        self.tokenizer = self.load_tokenizer()
         
         if self.huggingface_token is not None:
             huggingface_hub.login(token=self.huggingface_token)
@@ -29,13 +33,55 @@ class JutsuClassifier():
                 raise ValueError('Data path is required to train the model, since the model path does not exist in huggingface hub')
             
             train_data, test_data = self.load_data(self.data_path)
-            train_data_df = train_data.to_pandas()
+            train_data_df = train_data.to_pandas()   
             test_data_df = test_data.to_pandas()
             
             all_data = pd.concat([train_data_df, test_data_df]).reset_index(drop=True)
             class_weights = get_class_weights(all_data)
+            
+            self.train_model(train_data, test_data, class_weights)
+        
+        self.model = self.load_model(self.model_path)
     
-        self.tokenizer = self.load_tokenizer()
+    def train_model(self, train_data, test_data, class_weights):
+        model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels = self.num_labels, id2label = self.label_dict)
+        data_collator = DataCollatorWithPadding(tokenizer = self.tokenizer)
+        training_args = TrainingArguments(
+            output_dir=self.model_path,
+            learning_rate=2e-4,
+            per_device_train_batch_size=8,
+            per_device_test_batch_size=8,
+            num_train_epochs=5,
+            weight_decay=0.01,
+            evaluation_strategy='epoch',
+            logging_strategy='epoch',
+            push_to_hub=True,
+        )
+        
+        trainer = CustomTrainer(
+            model = model,
+            args = training_args,
+            data_collator = data_collator, 
+            training_dataset = train_data,
+            eval_dataset = test_data,
+            tokenizer = self.tokenizer,
+            compute_metrics = compute_metrics
+        )
+        
+        trainer.set_device(self.device)
+        trainer.set_class_weights(class_weights)
+        trainer.train()
+        
+        # Flush Memory
+        del trainer, model
+        gc.collect()
+        
+        if self.device == 'cuda':
+            torch.cuda.empty_cache()
+    
+    def load_model(self, model_path):
+        model = pipeline('text-classification', model=model_path, return_all_scores=True)
+        return model
     
     def load_tokenizer(self):
         if huggingface_hub.repo_exists(self.model_path):
@@ -44,7 +90,6 @@ class JutsuClassifier():
             return AutoTokenizer.from_pretrained(self.model_name)
     
     def load_data(self, data_path):
-        
         # read and pre-process the data
         df = pd.read_json(data_path, lines=True)
         df['jutsu_type_simplified'] = df['jutsu_type'].apply(self.simplify_jutsu)
@@ -89,3 +134,15 @@ class JutsuClassifier():
     
     def preprocess_function(self, tokenizer, examples):
         return tokenizer(examples['text_cleaned'], padding='max_length', truncation=True)
+    
+    def postprocess(self, model_output):
+        output = []
+        for pred in model_output:
+            label = max(pred, key=lambda x: x['score'])['label']
+            output.append(label)
+        return output
+    
+    def classify_jutsu(self, text):
+        model_output = self.model(text)
+        predictions = self.postprocess(model_output)
+        return predictions
